e73ef2b1dc1966d1961fac471044a7348ad1c87b
Nahit Ferhat <nahitektas@gmail.com>
2025-11-28 23:50:47 +0300

    gitignore

--- Switchly-2.0.WebApi/Entities/User.cs
+++ Switchly-2.0.WebApi/Entities/User.cs
@@ -2,5 +2,10 @@ namespace Switchly_2._0.WebApi.Entities;
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+    public string Email { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
+    public string? Name { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+
 }
