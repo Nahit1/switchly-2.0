68c70a03e00bc4704b5a0373959b186709c743bb
Nahit Ferhat <nahitektas@gmail.com>
2026-03-28 16:15:04 +0300

    .

---
+++ Switchly-2.0.WebApi/Context/Migrations/20260328131428_change-data-type.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Switchly_2._0.WebApi.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedatatype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000050"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000051"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000052"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlags",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000040"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "OrganizationMembers",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000011"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Organizations",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000010"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000030"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000031"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000032"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000020"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2026, 3, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000050"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000051"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlagEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000052"),
+                column: "UpdatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "FeatureFlags",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000040"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "OrganizationMembers",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000011"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Organizations",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000010"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000030"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000031"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ProjectEnvironments",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000032"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000020"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTimeOffset(new DateTime(2025, 12, 27, 13, 59, 23, 256, DateTimeKind.Unspecified).AddTicks(5590), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
